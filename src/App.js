b210544c2c44739fef19e1b6643b629e87e62592
Mike Schwartz <schwartzmpnyc@gmail.com>
2018-07-02 15:59:31 -0400

    Added dropdown component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import MasonryGallery from './components/masonry_component.js';
+import MasonryGallery from "./components/masonry_component.js";
+import FilterDropdown from "./components/filter_dropdown_component.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <FilterDropdown />
         <MasonryGallery />
       </div>
     );
---
+++ src/components/filter_dropdown_component.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default props => (
+	<select>
+		<option value="dev">Development</option>
+		<option value="ops">Operations</option>
+		<option value="sales">Sales</option>
+	</select>
+);
