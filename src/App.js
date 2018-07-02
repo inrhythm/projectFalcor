4d809177b2c057a2623a354e36d949ae5cc7a721
Mike Schwartz <schwartzmpnyc@gmail.com>
2018-07-02 16:16:21 -0400

    first prototype with images, people as objects. Drop down needs to set the state for our filter

--- src/App.js
+++ src/App.js
@@ -7,10 +7,26 @@ import FilterDropdown from "./components/filter_dropdown_component.js";
 
 class App extends Component {
   render() {
+    const people = [
+      {
+        src: "https://assets.time.com/assets/desktop-loop-poster.jpg",
+        team: "dev"
+      },
+      {
+        src:
+          "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=350",
+        team: "ops"
+      },
+      {
+        src:
+          "https://www.alvinailey.org/sites/default/files/styles/slideshow_image/public/melanie-person.jpg?itok=ocw3xkx_",
+        team: "sales"
+      }
+    ];
     return (
       <div className="App">
         <FilterDropdown />
-        <MasonryGallery />
+        <MasonryGallery people={people} />
       </div>
     );
   }
