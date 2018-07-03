0d52254cebefec6fa9aace1c2d65d66b665f23b3
Mike Schwartz <schwartzmpnyc@gmail.com>
2018-07-03 10:18:53 -0400

    Filtered out people now remain on page and will just kind of fade out a little

--- src/components/masonry_component.js
+++ src/components/masonry_component.js
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 import Masonry from "react-masonry-component";
 
 const masonryOptions = {
-	transitionDuration: 1000
+	transitionDuration: 0
+	// transitionDuration: 1000
 };
 
 class Gallery extends React.Component {
 	render() {
+		console.log(this.props.people)
 		const childElements = this.props.people.map(person => (
-			<img className="image-element-class" src={person.image1} />
+			<img className={"image-element-class" + (person.greyedOut ? " greyed-out" : "")} src={person.image1} />
 		));
 
 		return (
