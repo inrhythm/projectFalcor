ad3a8892bd14aaec20067339c0af583020e6c3c6
Mike Schwartz <schwartzmpnyc@gmail.com>
2018-07-03 12:03:51 -0400

    Added person's name and turned element into a link

--- src/components/masonry_component.js
+++ src/components/masonry_component.js
@@ -6,8 +6,11 @@ class Gallery extends React.Component {
 	render() {
 		const childElements = this.props.people.map(person => (
 			<div className={"image-container "+ (!person.greyedOut ? "brought-to-front" : "sent-to-rear")}>
+			<a className="person" href="#">
 				<Image className={"main-img image-element-class " + (person.greyedOut ? " greyed-out" : "not-greyed-out")} src={person.image1} />
 				<Image className={"hover-img image-element-class " + (person.greyedOut ? " greyed-out" : "not-greyed-out")} src={person.image2} />
+				<div className="person-name">{person.name}</div>
+			</a>
 			</div>
 		));
 
