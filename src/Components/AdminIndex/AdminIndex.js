663aacb6fd54f957253a1856360d10118e210e5f
surya <surya.mylar@gmail.com>
2018-07-05 15:30:43 -0400

    initial adminIndex

---
+++ src/Components/AdminIndex/AdminIndex.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AdminIndex extends React.Component {
+	render() {
+      return (
+			<div class="row">
+			  <div class="col-md-4">.col-md-4</div>
+			  <div class="col-md-4">.col-md-4</div>
+			  <div class="col-md-4">.col-md-4</div>
+			</div>
+      );
+   }
+}
+
+export default AdminIndex;
