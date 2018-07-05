152321888ea628c4bf98ef5edc39c093693523f6
Mike Schwartz <schwartzmpnyc@gmail.com>
2018-07-05 12:45:13 -0400

    In progress styling. Issues with image sizing

--- src/components/filter_dropdown_component.js
+++ src/components/filter_dropdown_component.js
@@ -5,11 +5,14 @@ export default props => {
 	const changeHandler = event => props.setFilter((event.target).value)
 
 	return (
-		<select className="filter-dropdown" id="filterDropdown" onChange={changeHandler}>
-			<option value="all">Everyone</option>
-			<option value="dev">Development</option>
-			<option value="ops">Operations</option>
-			<option value="sales">Sales</option>
-		</select>
+		<div className="filter-dropdown-div">
+			<label>Take a look at all our awesome people</label> 
+			<select className="filter-dropdown" id="filterDropdown" onChange={changeHandler}>
+				<option value="all">Everyone</option>
+				<option value="dev">Development</option>
+				<option value="ops">Operations</option>
+				<option value="sales">Sales</option>
+			</select>
+		</div>
 	)
 };
--- src/components/masonry_component.js
+++ src/components/masonry_component.js
@@ -5,17 +5,19 @@ import Image from './image-component.js';
 class Gallery extends React.Component {
 	render() {
 		const childElements = this.props.people.map(person => (
-			<div className={"image-container "+ (!person.greyedOut ? "brought-to-front" : "sent-to-rear")}>
-			<a className="person" href="#">
-				<Image className={"main-img image-element-class " + (person.greyedOut ? " greyed-out" : "not-greyed-out")} src={person.image1} />
-				<Image className={"hover-img image-element-class " + (person.greyedOut ? " greyed-out" : "not-greyed-out")} src={person.image2} />
-				<div className="person-name">{person.name}</div>
-			</a>
+			<div className={"image-container "+ (!person.greyedOut ? "brought-to-front" : "sent-to-rear")+" col-sm-2 col-lg-4"}>
+				<a className="person" href="#">
+					<div className="image-container">
+						<Image className={"main-img image-element-class " + (person.greyedOut ? " greyed-out" : "not-greyed-out")} src={person.image1} />
+						<Image className={"hover-img image-element-class " + (person.greyedOut ? " greyed-out" : "not-greyed-out")} src={person.image2} />
+					</div>
+					<div className="person-name">{person.name}</div>
+				</a>
 			</div>
 		));
 
 		return (
-			<div className="people-grid"> {childElements} </div>
+			<div className="people-grid container"> {childElements} </div>
 		);
 	}
 }
