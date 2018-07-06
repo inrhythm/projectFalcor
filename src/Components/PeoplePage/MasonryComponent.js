fe4fb70d018ba23a05bfec24f210242c0b210cf6
Mike Schwartz <schwartzmpnyc@gmail.com>
2018-07-06 13:03:01 -0400

    Another small change based on screen sizes

--- src/Components/PeoplePage/MasonryComponent.js
+++ src/Components/PeoplePage/MasonryComponent.js
@@ -5,7 +5,7 @@ import Image from './ImageComponent.js';
 class Gallery extends React.Component {
 	render() {
 		const childElements = this.props.people.map(person => (
-			 <div className={"person-container "+ (!person.greyedOut ? "brought-to-front" : "sent-to-rear")+"col-md-4 col-lg-3"}>
+			 <div className={"person-container "+ (!person.greyedOut ? "brought-to-front" : "sent-to-rear")+"col-md-4 col-lg-3 col-xl-2"}>
 				<a className="person" href="#">
 					<div className="image-container">
 						<Image className={"main-img image-element-class " + (person.greyedOut ? " greyed-out" : "not-greyed-out")} src={person.image1} />
