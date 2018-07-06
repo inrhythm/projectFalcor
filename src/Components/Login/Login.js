63685d948a16973a08a2311671ed382bfeff1eb8
Joe Cianflone <joe@cianflone.co>
2018-07-06 11:27:52 -0400

    :boom: login page start

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Login.scss';
+
+class Login extends React.Component {
+
+   render() {
+      return (
+         <div className="login">
+            <h1>Login Page</h1>
+         </div>
+      )
+   }
+}
+
+export default Login;
--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import People from './Components/People/People'
 import PersonEdit from './Components/PersonEdit/PersonEdit'
+import Login from './Components/Login/Login'
 
 const Router = () => (
 	<Switch>
 	  <Route exact path='/' component={People}/>
-	  <Route path='/register' component={PersonEdit}/>
+     <Route path='/register' component={PersonEdit}/>
+     <Route path='/login' component={Login}/>
 	</Switch>
 )
 
