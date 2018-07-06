f0c554f47c26a62e8b15c90f149d8efee7c55ec2
Mike Schwartz <schwartzmpnyc@gmail.com>
2018-07-06 13:13:45 -0400

    Fixed some issues from the merge

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import People from './Components/People/People'
+import People from './Components/PeoplePage/PeoplePage'
 import PersonEdit from './Components/PersonEdit/PersonEdit'
 
 const Router = () => (
