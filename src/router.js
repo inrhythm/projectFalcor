fd5dbfe3a5cfb7fc08db103a00577a2fe2eed0dc
surya <surya.mylar@gmail.com>
2018-07-06 11:20:50 -0400

    initial admin page

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import People_page from './Components/People/people_page'
 import PersonEdit from './Components/PersonEdit/PersonEdit'
+import AdminIndex from './Components/AdminIndex/AdminIndex'
 
 const Router = () => (
 	<Switch>
 	  <Route exact path='/' component={People_page}/>
 	  <Route path='/register' component={PersonEdit}/>
+	  <Route path='/admin' component={AdminIndex}/>
 	</Switch>
 )
 
