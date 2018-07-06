64dac038688a65ff18891259995c2383a7373e03
Joe Cianflone <joe@cianflone.co>
2018-07-06 12:50:36 -0400

    adds some updates to the styles

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,8 +5,23 @@ class Login extends React.Component {
 
    render() {
       return (
-         <div className="login">
-            <h1>Login Page</h1>
+         <div className="container--max">
+            <div className="login col-sm-10 col-md-8 col-wd-6">
+               <div className="container">
+                  <h1 className="login__header col-10">Welcome, Please Log In</h1>
+                  <form className="login__container col-10" action="">
+                     <label htmlFor="" className="input">
+                        <input name="username" type="text" required/>
+                     </label>
+                     <label htmlFor="" className="input">
+                        <input name="password" type="password" required/>
+                     </label>
+                     <div className="login__buttons">
+                        <button className="button--prime">Login</button>
+                     </div>
+                  </form>
+               </div>
+            </div>
          </div>
       )
    }
