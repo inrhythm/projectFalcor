6e647c2049df9f11dca0b563ead5b4cf82990ffa
Mike Schwartz <schwartzmpnyc@gmail.com>
2018-07-06 10:08:28 -0400

    figured out some spacing stuff with position absolute

--- src/Components/People/masonry_component.js
+++ src/Components/People/masonry_component.js
@@ -1,31 +1,23 @@
 import React, { Component } from "react";
-import Image from './image-component.js';
 
-class Gallery extends Component {
+import Image from './ImageComponent.js';
+
+class Gallery extends React.Component {
 	render() {
 		const childElements = this.props.people.map(person => (
-<<<<<<< HEAD:src/Components/People/masonry_component.js
-			<div className={"image-container "+ (!person.greyedOut ? "brought-to-front" : "sent-to-rear")}>
-			<a className="person">
-				<Image className={"main-img image-element-class " + (person.greyedOut ? " greyed-out" : "not-greyed-out")} src={person.image1} />
-				<Image className={"hover-img image-element-class " + (person.greyedOut ? " greyed-out" : "not-greyed-out")} src={person.image2} />
-				<div className="person-name">{person.name}</div>
-			</a>
-=======
-			<div className={"image-container "+ (!person.greyedOut ? "brought-to-front" : "sent-to-rear")+" col-sm-2 col-lg-4"}>
+			 <div className={"person-container "+ (!person.greyedOut ? "brought-to-front" : "sent-to-rear")+"col-md-4 col-lg-3"}>
 				<a className="person" href="#">
 					<div className="image-container">
 						<Image className={"main-img image-element-class " + (person.greyedOut ? " greyed-out" : "not-greyed-out")} src={person.image1} />
 						<Image className={"hover-img image-element-class " + (person.greyedOut ? " greyed-out" : "not-greyed-out")} src={person.image2} />
 					</div>
-					<div className="person-name">{person.name}</div>
+					<div className="person-name">{`${person.name} ${person.department}`}</div>
 				</a>
->>>>>>> In progress styling. Issues with image sizing:src/components/masonry_component.js
 			</div>
 		));
 
 		return (
-			<div className="people-grid container"> {childElements} </div>
+			<div className="people-grid"> {childElements} </div>
 		);
 	}
 }
